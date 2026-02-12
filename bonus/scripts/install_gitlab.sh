a0f25e33a6e3858da7455d6593adcb2ca3f8ec20
Akadil <akadil.kalimoldayev@gmail.com>
2026-02-12 17:09:08 +0100

    added a script to create a gitlab release

--- bonus/scripts/install_gitlab.sh
+++ bonus/scripts/install_gitlab.sh
@@ -13,7 +13,12 @@ helm install gitlab gitlab/gitlab \
   --set global.hosts.domain=DOMAIN \
   --set certmanager-issuer.email=me@example.com
 
+helm upgrade --install gitlab gitlab/gitlab -n gitlab -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml --set global.hosts.domain=k3d.gitlab.com  --set global.hosts.externalIP=0.0.0.0   --set global.hosts.https=false   --timeout 600s --debug --wait-for-jobs
+
 # so reqs are:
 # 1. DOMAIN - probably domain in cluster, but as I know it should also be 
 # 	accessible outside of it
-# 2. Just any email. For identification purposes I gues
+# 2. Just any email. For identification purposes I guess
+#
+kubectl port-forward -n gitlab svc/gitlab-webservice-default 8181:8080
+
